7c53d67e6b391b3de5440583a2648b125cd41771
Shirin <kotyrowashirin@gmail.com>
2021-12-03 22:10:13 +0300

    back button

--- src/components/head.vue
+++ src/components/head.vue
@@ -10,31 +10,45 @@
                 background-color="#EFF0F8"
                 dense
         ></v-text-field>
-        <div>
-            <div>
+        <div class= "d-flex">
+               <a href="/marks" style="color: #292b61; font-family: gilroy-reguar;">
+            <div >
                 <div style="display:inline-block;margin-top:14px;float:left; font-family: gilroy-reguar; margin-right:20px">
-                    Мои оценки     &nbsp;&nbsp;&nbsp;  10
+                  Мои оценки     &nbsp;&nbsp;&nbsp;  10
                 </div>
                 <div style="display:inline-block;margin-top:-6px;margin-left:-20px">
                     <img :src="require('@/assets/img/star.svg')" style="width:60px;height:60px;">
                 </div>
             </div>
+            </a> 
+             <a href="/achieves" style="color: #292b61; font-family: gilroy-reguar;">
             <div>
                 <div style="display:inline-block;margin-top:14px;float:left">
-                   <a href="/achieves" style="color: #292b61; font-family: gilroy-reguar;">Мои достижения</a> 
+                  Мои достижения
                 </div>
                 <div style="display:inline-block;margin-top:-6px;margin-left:-20px">
                     <img :src="require('@/assets/img/achieve.svg')" >
                 </div>
             </div>
+            </a> 
         </div>
+        <img v-if="currentRoute != '/home'" type="button" :src="require('@/assets/img/Back.svg')" @click="$router.go(-1)">
     </div>
 </template>
 
 <script>
     export default {
-        name: "headr"
+        name: "headr",
+    mounted(){
+    },
+    computed: {
+        currentRoute(){
+          this.$router.currentRoute.path
+          return window.location.pathname
+        }
     }
+}
+
 </script>
 
 <style scoped>
